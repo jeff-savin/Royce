a1594102ebe570273585f90860c73147b9d27d7c
Jeff Savin <jeff.savin@varsitytutors.com>
2015-07-28 16:09:28 -0700

    Use superclass if exists on named scopes

--- lib/royce/class_methods.rb
+++ lib/royce/class_methods.rb
@@ -15,8 +15,12 @@ module Royce
         # Add scopes to including class
         # User.admins
         # User.editors
-        available_role_names.each do |role_name|
+        if includer.superclass == ActiveRecord::Base
           includer_class_name = includer.model_name.to_s.underscore.pluralize
+        else
+          includer_class_name = includer.superclass.model_name.to_s.underscore.pluralize
+        end
+        available_role_names.each do |role_name|
           scope role_name.pluralize, -> { Role.find_by(name: role_name).send includer_class_name.to_sym }
         end
 
@@ -28,7 +32,7 @@ module Royce
         if includer.superclass == ActiveRecord::Base
           has_many includer.model_name.to_s.underscore.pluralize.to_sym, through: :connectors, source: :roleable, source_type: includer.model_name.to_s
         else
-          has_many includer.model_name.to_s.underscore.pluralize.to_sym, through: :connectors, source: :roleable, source_type: includer.superclass.model_name.to_s
+          has_many includer.superclass.model_name.to_s.underscore.pluralize.to_sym, through: :connectors, source: :roleable, source_type: includer.superclass.model_name.to_s
         end
       end
 
